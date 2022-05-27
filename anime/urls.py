d0b2b0a26a5be999db4d3b875a01e95444969174
Zacharea <Zachareakaawan2@gmail.com>
2022-05-26 23:32:44 -0400

    Added Delete Anime View

--- anime/urls.py
+++ anime/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import AnimeListView
+from .views import AnimeDeleteView, AnimeListView
+from anime import views
 
 urlpatterns = [
     path('', AnimeListView.as_view(), name='anime-home'),
     # path('genre/<genre>', AnimeHome.as_view(), name='anime-home'),
+    path('delete/<int:pk>', AnimeDeleteView.as_view(), name='anime-delete'),
+    
 
 ]
--- anime/views.py
+++ anime/views.py
@@ -1,10 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.views import View
-from django.views.generic import ListView
+from django.views.generic import ListView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Anime
 # Create your views here.
 
 
 class AnimeListView(ListView):
     model = Anime
+
+class AnimeDeleteView(LoginRequiredMixin, DeleteView):
+    model = Anime
+
+    success_url = '/anime'
+
+
+# def AnimeDeleteView(request, id):
+#     anime = Anime.objects.get(id=id)
+#     anime.delete()
+#     return redirect('/anime/')
